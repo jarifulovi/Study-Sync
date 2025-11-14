8ecada4847db53dfe4b84b6f65e6bf8cb8a76700
jarifulovi <jarifulovi@gmail.com>
2025-11-14 21:53:28 +0600

    feat: integrate Supabase client for backend operations and add necessary dependencies

--- frontend-nextjs/src/backend/lib/supabaseServer.ts
+++ frontend-nextjs/src/backend/lib/supabaseServer.ts
@@ -2,14 +2,10 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseServiceKey = process.env.NEXT_SECRET_SUPABASE_SERVICE_ROLE_KEY!
 
+// This is supabase with admin access
+export const supabaseService = createClient(supabaseUrl, supabaseServiceKey);
 
 // Create Supabase client for server-side operations (API routes)
-export const createServerSupabaseClient = () => {
-  return createClient(supabaseUrl, supabaseAnonKey, {
-    auth: {
-      autoRefreshToken: false,
-      persistSession: false
-    }
-  });
-}
+export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
